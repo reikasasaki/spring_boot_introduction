0ca71dad1ef019de2e7d1e29081ba9299e1dcc7d
sasakireika <bknb1743rmsr@gmail.com>
2023-11-02 19:20:18 +0900

    Lesson03実装完了

--- src/main/java/com/example/controller/LessonController.java
+++ src/main/java/com/example/controller/LessonController.java
@@ -1,36 +1,18 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/lesson")
 public class LessonController {
 
 	@GetMapping("/sample")
-	@ResponseBody
-	public String sample() {
-		return "Hello World!";
-	}
-	
-	@GetMapping("/")
-	@ResponseBody
-	public String index() {
-		return "Hello World!";
-	}
-	
-	@GetMapping("/test")
-	@ResponseBody
-	public String test() {
-		return "Good Evening!";
-	}
-	
-	@GetMapping("/test/{num}")
-	@ResponseBody
-	public String test(@PathVariable Integer num) {
-		return "引数の数は：" + num ;
+	public String sample(Model model) {
+		String text = "Hello Spring Boot!!";
+		model.addAttribute("message", text);
+		return "index";
 	}
 }
