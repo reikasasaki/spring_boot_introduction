9e401e8c23c992336c575c5f27d0b97a48985cbe
sasakireika <bknb1743rmsr@gmail.com>
2023-11-07 14:11:39 +0900

    Lesson04実装完了

--- src/main/java/com/example/controller/LessonController.java
+++ src/main/java/com/example/controller/LessonController.java
@@ -3,7 +3,12 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.example.form.SampleForm;
+
 
 @Controller
 @RequestMapping("/lesson")
@@ -15,4 +20,16 @@ public class LessonController {
 		model.addAttribute("message", text);
 		return "index";
 	}
+	
+	@PostMapping("/request_test")
+	@ResponseBody
+	public String postTest(SampleForm sampleForm) {
+		return "名前: " + sampleForm.getName() + "<br>血液型: " + sampleForm.getBloodType();
+	}
+	
+	@GetMapping("/form_test")
+	public String formTest(@ModelAttribute SampleForm sampleForm) {
+		return "lesson/form_test";
+	}
+	
 }
